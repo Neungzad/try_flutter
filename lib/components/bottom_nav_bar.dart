b08245401ce4013b4d149bc3742476851bf67f26
neungszad <neungszad@gmail.com>
2024-08-23 07:32:46 +0700

    feat: Update dependencies and add bottom navigation bar to home page

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final void Function(int) onTabChange;
+  const BottomNavBar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: GNav(
+        tabActiveBorder: Border.all(
+          color: Colors.white,
+        ),
+        color: Colors.grey[400],
+        activeColor: Colors.grey.shade700,
+        tabBackgroundColor: Colors.grey.shade100,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: 'Shop',
+            gap: 4, // Add margin between icon and text
+          ),
+          GButton(
+            icon: Icons.shop,
+            text: 'Cart',
+            gap: 4, // Add margin between icon and text
+          ),
+        ],
+        onTabChange: (index) => onTabChange(index),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:try_flutter/components/bottom_nav_bar.dart';
+import 'package:try_flutter/pages/cart_page.dart';
+import 'package:try_flutter/pages/shop_page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _pageIndex = 0;
+
+  void navigateBottomNavBar(int index) {
+    setState(() {
+      _pageIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => navigateBottomNavBar(index),
+      ),
+      body: _pages[_pageIndex],
     );
   }
 }
